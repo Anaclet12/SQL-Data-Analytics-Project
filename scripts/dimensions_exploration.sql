c6756e8ece37d99d2f1fef60253a62bf8baa65e9
Anaclet Sado <168388863+Anaclet12@users.noreply.github.com>
2025-10-23 03:59:04 -0400

    Update dimensions_exploration.sql

--- scripts/dimensions_exploration.sql
+++ scripts/dimensions_exploration.sql
@@ -10,3 +10,14 @@ SQL Functions Used:
     - ORDER BY
 ===============================================================================
 */
+-- Explore all countries our customers come from
+SELECT DISTINCT country
+FROM gold.dim_customers
+ORDER BY 1
+/*They come from 6 countries*/
+
+-- Explore all categories
+SELECT DISTINCT category, SUBCATEGORY, product_name
+FROM gold.dim_products
+ORDER BY 1
+/*288 products in 36 subcategies grouped 4 categories*/
